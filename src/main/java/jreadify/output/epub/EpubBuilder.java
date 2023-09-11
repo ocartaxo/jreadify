3d01af1e2db8ecc05ce0b0afaa7d514d585fe611
ocartaxo <oacartax@gmail.com>
2023-09-11 11:52:34 -0300

    [SRP] gerador de ebooks de acordo com o formato

---
+++ src/main/java/jreadify/output/epub/EpubBuilder.java
@@ -0,0 +1,38 @@
+package jreadify.output.epub;
+
+import jreadify.domain.Chapter;
+import jreadify.domain.Ebook;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubWriter;
+import nl.siegmann.epublib.service.MediatypeService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class EpubBuilder {
+
+    public void build(Ebook ebook) {
+
+        var epub = new Book();
+
+        Path outputFileDir = ebook.outputFileDir();
+
+        for (Chapter chapter : ebook.chapters()) {
+            String html = chapter.getHtmlContent();
+            String chapterTitle = chapter.getTitle();
+
+            epub.addSection(chapterTitle, new Resource(html.getBytes(), MediatypeService.XHTML));
+        }
+
+        var epubWriter = new EpubWriter();
+        try {
+            epubWriter.write(epub, Files.newOutputStream(outputFileDir));
+        } catch (IOException ex) {
+            throw new IllegalStateException("Erro ao criar arquivo EPUB: " + outputFileDir.toAbsolutePath(), ex);
+        }
+
+    }
+
+}
---
+++ src/main/java/jreadify/output/pdf/PDFBuilder.java
@@ -0,0 +1,42 @@
+package jreadify.output.pdf;
+
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.IBlockElement;
+import com.itextpdf.layout.element.IElement;
+import com.itextpdf.layout.properties.AreaBreakType;
+import jreadify.domain.Ebook;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class PDFBuilder {
+
+    public void build(Ebook ebook) {
+
+        Path outputFilesDir = ebook.outputFileDir();
+
+        try(var writer = new PdfWriter(Files.newOutputStream(outputFilesDir));
+
+            var pdf = new PdfDocument(writer);
+            var pdfDocument = new Document(pdf)) {
+
+            ebook.chapters().forEach(chapter -> {
+                String html = chapter.getHtmlContent();
+                List<IElement> convertToElements = HtmlConverter.convertToElements(html);
+                convertToElements.forEach(element -> pdfDocument.add((IBlockElement) element));
+                if (!ebook.isLastChapter(chapter)) {
+                    pdfDocument.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+                }
+
+            });
+
+        } catch (Exception ex) {
+            throw new IllegalStateException("Erro ao criar arquivo PDF: " + ebook.outputFileDir().toAbsolutePath(), ex);
+        }
+    }
+}
