efdff352f9f311d369d3798e3f26c3eee1b83e9f
ocartaxo <oacartaxo@gmail.com>
2025-01-28 00:28:01 -0300

    [OCP] novo formato de ebook

--- src/main/java/jreadify/domain/EbookFormat.java
+++ src/main/java/jreadify/domain/EbookFormat.java
@@ -1,14 +1,16 @@
 package jreadify.domain;
 
 import jreadify.application.EbookAssembler;
-import jreadify.epub.EPUBAssembler;
-import jreadify.pdf.PDFAssembler;
+import jreadify.assembler.EPUBAssembler;
+import jreadify.assembler.HTMLAssembler;
+import jreadify.assembler.PDFAssembler;
 import lombok.Getter;
 
 @Getter
 public enum EbookFormat {
    PDF(new PDFAssembler()),
-   EPUB(new EPUBAssembler());
+   EPUB(new EPUBAssembler()),
+   HTML(new HTMLAssembler());
 
    private final EbookAssembler assembler;
 
