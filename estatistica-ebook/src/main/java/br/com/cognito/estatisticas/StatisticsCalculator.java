a183030cae8bf50afa39a164fac083a3e6988cde
Otavio Cartaxo <oacartaxo@gmail.com>
2026-07-30 00:21:10 -0300

    refactoring

--- estatistica-ebook/src/main/java/br/com/cognito/estatisticas/StatisticsCalculator.java
+++ estatistica-ebook/src/main/java/br/com/cognito/estatisticas/StatisticsCalculator.java
@@ -5,6 +5,8 @@ import jreadify.plugin.Plugin;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.text.Normalizer;
+
 public class StatisticsCalculator implements Plugin {
     @Override
     public String afterRender(String html) {
@@ -13,14 +15,32 @@ public class StatisticsCalculator implements Plugin {
 
     @Override
     public void afterAssemble(Ebook ebook) {
+
+        WordsCount wordsCount = new WordsCount();
+
         for (var c : ebook.chapters()) {
-            String html = c.getHtmlContent();
-            Document doc = Jsoup.parse(html);
-            String chapterText = doc.body().text();
-            String[] words = chapterText.split("\\s+");
-            for (var w : words) {
-                System.out.println(w);
+
+            String[] words = normalizeText(c.getHtmlContent()).split("\\s+");
+
+            for (final String word : words) {
+                wordsCount.addWord(word.toUpperCase());
             }
+
         }
+
+        for (var count : wordsCount.entrySet()) {
+            String word = count.getKey();
+            Integer freq = count.getValue();
+            System.out.println(word + ": " + freq);
+        }
+    }
+
+    private String normalizeText(String html) {
+        Document doc = Jsoup.parse(html);
+        String chapterText = doc.body().text();
+        String chapterTextWithoutPunc = chapterText.replaceAll("\\p{Punct}", " ");
+        return Normalizer
+                .normalize(chapterTextWithoutPunc, Normalizer.Form.NFD)
+                .replaceAll("[^\\p{ASCII}]", "");
     }
 }
